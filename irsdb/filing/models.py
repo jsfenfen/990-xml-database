1cf92e2687e4cef023058f024338f27d7a1bd6c7
Jacob Fenton <jsfenfen@gmail.com>
2018-05-02 15:20:56 -0700

    set schema version on filing object

--- irsdb/filing/models.py
+++ irsdb/filing/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.conf import settings
+import os
+import re
 
+from irsx import settings as irsx_settings
+XML_DIR = irsx_settings.WORKING_DIRECTORY
+
+VERSION_RE=re.compile(r'returnVersion="(20\d\dv\d\.\d)"')
 
 class Filing(models.Model):
 
@@ -37,6 +43,30 @@ class Filing(models.Model):
 
     def get_local_URL(self):
         return os.path.join(XML_DIR, "%s_public.xml" % self.object_id)
+
+    def set_schema_version(self):
+        """
+        Sets the schema version by trying to read top of file locally.
+        Efficient b/c it doesn't parse xml, just runs regex on files second line.
+        Doesn't set if file is missing.
+        """
+        filepath = self.get_local_URL()
+        try:
+            infile = open(filepath, "r")
+        except FileNotFoundError:
+            print("File %s is missing, quitting" % filepath)
+            return False
+        top = infile.read(1024)
+        infile.close()
+        returnline = top.split("\n")[1]
+        result = VERSION_RE.search(returnline)
+        if result:
+            if result != self.schema_version:
+                self.schema_version = result
+                self.save()
+        else:
+            print("No result in object_id: %s returnline:%s" % (self.object_id, returnline))
+
 
     class Meta:
         managed = True
